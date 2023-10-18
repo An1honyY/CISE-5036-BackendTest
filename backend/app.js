3fc1b24e88f25691f7a564113243267466ccea2f
An1honyY <ant.jo.yuen@gmail.com>
2023-10-18 21:58:45 +1300

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -1,23 +1,22 @@
-const serverless = require("serverless-http");
-const express = require("express");
+const express = require('express');
+const mongoose = require('mongoose');
+
 const app = express();
 
-app.get("/", (req, res, next) => {
-  return res.status(200).json({
-    message: "Hello from root!",
-  });
+// Middleware for JSON parsing
+app.use(express.json());
+
+// MongoDB connection
+mongoose.connect('mongodb+srv://user1:HFTGHEsrTYC8OJWt@cluster0.hvwapzz.mongodb.net/?retryWrites=true&w=majority', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
 });
 
-app.get("/hello", (req, res, next) => {
-  return res.status(200).json({
-    message: "Hello from path!",
-  });
+// Define your API routes and handlers
+app.get('/api/articles', (req, res) => {
+  // Handle your MongoDB queries and responses here
+  res.json({ message: 'Hello from MongoDB!' });
 });
 
-app.use((req, res, next) => {
-  return res.status(404).json({
-    error: "Not Found",
-  });
-});
-
-module.exports.handler = serverless(app);
+// Export the Express app
+module.exports = app;
