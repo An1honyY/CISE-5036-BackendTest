ae13f8c1c3b642013f6fe59d592e40041e6f25b1
An1honyY <ant.jo.yuen@gmail.com>
2023-10-21 22:48:53 +1300

    Update express

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,5 +1,4 @@
 const mongoose = require('mongoose');
-const config = require('config');
 const db = "mongodb+srv://user1:HFTGHEsrTYC8OJWt@cluster0.hvwapzz.mongodb.net/?retryWrites=true&w=majority";
 
 const connectDB = async () => {
