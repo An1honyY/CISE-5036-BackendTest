5c23c3f9e476f753fe2fb6723219c61013e69466
An1honyY <ant.jo.yuen@gmail.com>
2023-10-21 22:25:55 +1300

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -1,17 +1,30 @@
-const express = require('express');
 const mongoose = require('mongoose');
+const connectDB = require('./config/db');
+const cors = require('cors');
+
+// routes
+const articles = require('./routes/api/articles');
+const suggest = require('./routes/api/suggest')
+const moderate = require('./routes/api/moderate');
+
 const app = express();
+// Connect Database
 // Middleware for JSON parsing
-app.use(express.json());
-// MongoDB connection
-mongoose.connect('mongodb+srv://user1:HFTGHEsrTYC8OJWt@cluster0.hvwapzz.mongodb.net/?retryWrites=true&w=majority', {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-});
+app.use(express.json({ extended: false }));
+
 app.get('/', (req, res) => {
-    res.send('Hello world!');
+    res.send('SPEED Backend Running');
   });
-  
+
+// Define your API routes and handlers
+app.use('/api/articles', articles);
+app.use('/api/suggest', suggest);
+app.use('/api/moderate', moderate);
+
+
+const port = process.env.PORT || 8082;
+app.listen(port, () => console.log(`Server running on port ${port}`));
+
 
 // Export the Express app
 module.exports = app;
