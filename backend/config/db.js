d4525b86e3f2874e4990dd36a4be6a830eb659ca
An1honyY <ant.jo.yuen@gmail.com>
2023-10-21 22:36:04 +1300

    Update db.js

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,12 +1,13 @@
 const mongoose = require('mongoose');
-const config = require('config');
-const db = config.get('mongoURI');
+const config = require('default');
+const db = config.mongoURI;
 
 const connectDB = async () => {
   try {
     mongoose.set('strictQuery', true);
     await mongoose.connect(db, {
       useNewUrlParser: true,
+      useUnifiedTopology: true,
     });
 
     console.log('MongoDB is Connected...');
